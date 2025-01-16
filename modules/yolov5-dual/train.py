88ada58dfa4c3f4925bfc72992bac3b7c7df2278
jemmyalice <pjjemmy@gmail.com>
2025-01-16 20:19:25 +0800

    Update train.py

--- modules/yolov5-dual/train.py
+++ modules/yolov5-dual/train.py
@@ -11,6 +11,8 @@ Usage:
     $ python path/to/train.py --data coco128.yaml --weights yolov5s.pt --img 640  # from pretrained (RECOMMENDED)
     $ python path/to/train.py --data coco128.yaml --weights '' --cfg yolov5s.yaml --img 640  # from scratch
 """
+import warnings
+warnings.filterwarnings("ignore")
 
 import argparse
 import math
